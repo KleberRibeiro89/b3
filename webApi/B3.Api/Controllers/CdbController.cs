97f391d83b1f472174a90e89cd1892a608a2857a
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2023-06-24 21:27:51 -0300

    criação de testes de api de cálculos

---
+++ webApi/B3.Api/Controllers/CdbController.cs
@@ -0,0 +1,41 @@
+﻿using B3.Domain.Commands.Inputs;
+using B3.Domain.Commands.Results;
+using B3.Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B3.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CdbController : ControllerBase
+    {
+        private readonly ICdbService _service;
+
+        public CdbController(ICdbService service)
+        {
+            _service = service;
+        }
+
+
+
+
+        [HttpPost("calcular")]
+        public IActionResult Calcular(CalcularCdbCommand command)
+        {
+            try
+            {
+               var result = _service.Calcular(command);
+                if (!command.IsValid)
+                    return BadRequest(result);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+            
+        }
+    }
+}
--- webApi/B3.Api/Program.cs
+++ webApi/B3.Api/Program.cs
@@ -1,3 +1,6 @@
+using B3.Domain.Services.Implementation;
+using B3.Domain.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<ICdbService, CdbService>();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
