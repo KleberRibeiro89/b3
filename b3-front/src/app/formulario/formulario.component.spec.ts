cfd893574cad927cf67350c44c877740879fa078
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2023-06-26 12:58:29 -0300

    ajustes teste front

--- b3-front/src/app/formulario/formulario.component.spec.ts
+++ b3-front/src/app/formulario/formulario.component.spec.ts
@@ -3,10 +3,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormularioComponent } from './formulario.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { CdbService } from '../services/cdb.service';
 
 describe('FormularioComponent', () => {
   let component: FormularioComponent;
   let fixture: ComponentFixture<FormularioComponent>;
+  let service: CdbService;
 
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule, FormsModule],
@@ -19,19 +21,51 @@ describe('FormularioComponent', () => {
     fixture.detectChanges();
   });
 
-  it('deve calcular corretamente o  total', () => {
-    component.model.prazo = 6;
-    component.model.valorAplicado = 100;
+  it('Solicitar Calculo de Cdb Com valor Positivo', () => {
+    component.model.prazo = 2;
+    component.model.valorAplicado = 1;
 
-    component.calcular();
-    expect(component.result.bruto).toBe(100);
+    service.calcularCDB(component.model);
+    expect(component.result.bruto > 0).toBeTrue();
   });
 
-  it('deve exibir o total no template', () => {
+  it('Solicitar Calculo de Cdb Com valor Negativo', () => {
+    component.model.prazo = 6;
+    component.model.valorAplicado = -1;
+
+    service.calcularCDB(component.model);
+    expect(component.result.liquido).toBe(0);
+  });
+
+  it('Solicitar Calculo de Cdb Com Mes igual á 1', () => {
+    component.model.prazo = 1;
+    component.model.valorAplicado = 1;
+
+    service.calcularCDB(component.model);
+    expect(component.result.liquido).toBe(0);
+  });
+
+  it('Testar Cálculo, resultado Líquido', () => {
     component.model.prazo = 6;
     component.model.valorAplicado = 100;
 
-    component.calcular();
-    expect(component.result.liquido).toBe(100);
+    service.calcularCDB(component.model);
+    expect(component.result.liquido).toBe(105.01);
+  });
+
+  it('Testar Cálculo, resultado Líquido', () => {
+    component.model.prazo = 12;
+    component.model.valorAplicado = 100;
+
+    service.calcularCDB(component.model);
+    expect(component.result.liquido).toBe(109.88);
+  });
+
+  it('Testar Cálculo, resultado Bruto', () => {
+    component.model.prazo = 6;
+    component.model.valorAplicado = 100;
+
+    service.calcularCDB(component.model);
+    expect(component.result.bruto).toBe(106.47);
   });
 });
